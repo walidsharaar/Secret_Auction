b53aba172aa2811ea935399073984fe982ec1338
Sharaar <walidsharar@gmail.com>
2021-06-02 19:26:27 +0200

    logic mapped

--- main.py
+++ main.py
@@ -1,16 +1,58 @@
-# This is a sample Python script.
+#import library
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import os
+
+from  asciis import welcome_massege
+print(welcome_massege)
+
+print("Welcome to the secret auction program.")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+#todo clear the terminal
+
+def clear():
+    os.system('cls')
+#todo create function to find highest bidder
+
+def find_bidder():
+    highest_amount=0
+
+    #{"a":123}
+    for bidder in bidding:
+        highest_amount= bidding[bidder]
+        if highest_amount > highest_amount:
+            highest_amount = bid_amount
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#todo create a empty dictory.
+
+bids={}
+bidding_closed=False
+
+
+
+#todo while loop to get user input
+
+while not bidding_closed:
+    name = input("What is your name? ")
+    bid = int(input("What is your bid? $"))
+    bids[name]= bid
+    close_the_bid = input("Do you want to close the bed? Type 'Y' for yes and 'N' for no").lower()
+    if close_the_bid =="no":
+        bidding_closed=True
+    elif close_the_bid=="yes":
+        bidding_closed=False
+        clear()
+    else:
+        close_the_bid=True
+
+
+
+
+
+
+clear()
+
+

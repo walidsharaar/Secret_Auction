6c0a47b730bf633c256887c269b7ade7665f5c4f
Sharaar <walidsharar@gmail.com>
2021-06-02 20:18:12 +0200

    Challenge completed

--- main.py
+++ main.py
@@ -1,58 +1,47 @@
-#import library
-
+#todo import the os library for clearing terminal
 import os
 
-from  asciis import welcome_massege
+#todo print the ASCII message to the user
+from asciis import welcome_massege
 print(welcome_massege)
-
 print("Welcome to the secret auction program.")
-
-
 
 #todo clear the terminal
 
 def clear():
     os.system('cls')
+
 #todo create function to find highest bidder
-
-def find_bidder():
-    highest_amount=0
-
-    #{"a":123}
-    for bidder in bidding:
-        highest_amount= bidding[bidder]
-        if highest_amount > highest_amount:
-            highest_amount = bid_amount
-
-
-
-#todo create a empty dictory.
-
 bids={}
-bidding_closed=False
+bidding_closed = False
+
+def find_bidder(biddin_record):
+    highest_amount=0
+    winner = ""
+    #biddings={"a":123,"b":321}
+
+    for bidder in biddin_record:
+        amount= biddin_record[bidder]
+        if amount > highest_amount:
+            highest_amount = amount
+            winner = bidder
+    print(f"This winner for this bidding is {winner} with a bid of $ {amount}")
 
 
+#todo create a empty dictory and while loop to get user input
 
-#todo while loop to get user input
 
 while not bidding_closed:
-    name = input("What is your name? ")
-    bid = int(input("What is your bid? $"))
-    bids[name]= bid
-    close_the_bid = input("Do you want to close the bed? Type 'Y' for yes and 'N' for no").lower()
-    if close_the_bid =="no":
-        bidding_closed=True
-    elif close_the_bid=="yes":
-        bidding_closed=False
+    name = input("What is your name?: ")
+    price = int(input("What is your bid?: $"))
+    bids[name] = price
+    should_continue = input("Are there any other bidders? Type 'yes or 'no'.\n")
+    if should_continue == "no":
+        bidding_closed = True
+        find_bidder(bids)
+    elif should_continue == "yes":
         clear()
-    else:
-        close_the_bid=True
 
 
-
-
-
-
-clear()
 
 
